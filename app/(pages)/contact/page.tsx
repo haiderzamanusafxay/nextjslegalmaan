5c48dbc1cc4ade8e34c1cdfdc25e498d974b8106
Haider Zaman <haiderzamanusafxay@gmail.com>
2023-11-24 14:27:01 +0500

    made some changes

--- app/(pages)/contact/page.tsx
+++ app/(pages)/contact/page.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
 
 const Contact = () => {
   return (
-    <form action="https://fabform.io/f/{form-id}" method="post">
+    <form method="post">
       <section className="text-gray-600 body-font relative lg:gap-x-8 lg:px-8">
         <div className="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
           <div className="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
